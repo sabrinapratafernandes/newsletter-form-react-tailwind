f3d546740ea9e2e5c173b97923f3750fc4a7e822
Sabrina <sabrinabuenoprata@gmail.com>
2023-07-26 21:09:25 -0300

    ajuste-input

--- src/components/NewsletterForm.js
+++ src/components/NewsletterForm.js
@@ -1,22 +1,50 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 // Componente NewsletterForm
 const NewsletterForm = () => {
   // Estados para armazenar o valor do email, se houver erro e se houver sucesso
   const [email, setEmail] = useState(''); // Estado para o valor do email digitado pelo usuário
   const [error, setError] = useState(false); // Estado para controlar a exibição da mensagem de erro
-  const [success] = useState(false); // Estado para controlar a exibição da mensagem de sucesso
+  const [success, setSuccess] = useState(false); // Estado para controlar a exibição da mensagem de sucesso
+
 
   // Função para lidar com o envio do formulário
-  const handleSubmit = (e) => {
+  const handleSubmit = useCallback((e) => {
+    e.preventDefault();
+
+    const data = new FormData(e.target)
+
+    console.log(data.get('email'))
+    if (error) {
+      return 
+    }
+
+    setTimeout(() => {
+      setError(false);
+      setSuccess(true);
+      setEmail(''); // e.target.querySelector('[name="email"]').value = '' | Outra Opção que otimizaria a performace
+    }, 1500);
+
+  }, [error]);
+
+  useEffect( () => {
+    if(error){
+      setSuccess(false)
+    }
+  }, [error])
+
+  const isValidEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
   };
+
+
 
   // Retorna a interface do componente com o formulário e as mensagens de erro/sucesso
   return (
     <form 
         onSubmit={handleSubmit} 
         onInvalid={ () => setError(true) }
-        
         className="flex flex-col text-blue-dark text-xs"
     >
       <span className="
@@ -29,9 +57,13 @@ const NewsletterForm = () => {
       {/* Campo de input para o email */}
       <input
         type="email"
+        name="email"
         placeholder="email@company.com"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)} // Atualiza o estado do email conforme o usuário digita o email
+        // value={email}
+        onChange={(e) => {
+          setError(false)
+          // setEmail(e.target.value)
+        }} // Atualiza o estado do email conforme o usuário digita o email
         className={`
           border 
           p-2 
@@ -44,17 +76,10 @@ const NewsletterForm = () => {
         
       />
       
-      {error && 
-        <p className="text-red-500 mt-2">
-          Valid email Required | Insira um email válido.
-        </p>
-      }
+      {error && <p className="text-red-500 mt-2">Valid email Required | Insira um email válido.</p>}
+      {success && <p className="text-green-500 mt-2">Inscrição realizada com sucesso!</p>}
       
-      {success && 
-        <p className="text-green-500 mt-2">
-          Inscrição realizada com sucesso!
-        </p>
-      }
+     
       <button
         type="submit"
         className="
